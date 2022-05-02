032b0224966a13383e42286ab6d25ed31ec7dad4
許唐銘 <tmhsu@TMHsu-MBA.local>
2022-05-02 17:37:36 +0800

    Food order init (without state change)

---
+++ src/components/Layout/Header.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Fragment } from "react";
+import HeaderCartButton from "./HeaderCartButton";
+import mealImage from "../../assets/meals.jpeg";
+import classes from "./Header.module.css";
+
+const Header = (props: any) => {
+  return (
+    <Fragment>
+      <header className={classes.header}>
+        <h1>ReactMeals</h1>
+        <HeaderCartButton onClick={props.onShowCart}/>
+      </header>
+      <div className={classes["main-image"]}>
+        <img src={mealImage} alt="A table full of food" />
+      </div>
+    </Fragment>
+  );
+};
+
+export default Header;
